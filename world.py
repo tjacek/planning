eaecb9ea1e9b783eab4cc0d53175af8ebf80a2a6
tjacek <tjacek@agh.edu.pl>
2020-09-13 13:57:31 +0200

    show polygon

--- world.py
+++ world.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 
 class ConvexPolygon(object):
     def __init__(self,half_planes):
@@ -17,13 +18,20 @@ class HalfPlane(object):
         f=np.sum(self.coff[:-1]*point)
         return (f+self.coff[-1])>0.0
 
-def make_point(x,y):
-    return np.array([x,y])
+def plot_polygon(polygon,n=100,scale=100):
+    points=scale*np.random.random_sample((n,2))
+    points=[ point_i
+                for point_i in points
+                    if(polygon(point_i))]
+    print(len(points))
+    plt.figure()
+    ax = plt.subplot(111)
+    points=np.array(points)
+    plt.scatter(points[:,0],points[:,1])
+    plt.show()
 
-#plane=HalfPlane(0.0,1.0,-1.0)
-#print(plane(make_point(1.0,1.0)))
+
 a=[ HalfPlane(1.0,1.0,1.0),HalfPlane(1.0,1.0,-1.0),
     HalfPlane(-1.0,1.0,1.0),HalfPlane(-1.0,1.0,-1.0)]
 polygon=ConvexPolygon(a)
-print(polygon(make_point(0.0,100.0)))
-
+plot_polygon(polygon,n=1000)
