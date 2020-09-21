7a67e003f53912c623052d040b9c1c9ca50b689d
tjacek <tjacek@agh.edu.pl>
2020-09-21 21:29:09 +0200

    next state implemented

--- grid.py
+++ grid.py
@@ -12,22 +12,47 @@ class GridSearch(search.FowardSearch):
             return False	
         self.goal=self.get_state(end)
         start=self.get_state(start)
-        super().__call__(start)	
+        return super().__call__(start)	
 
     def is_goal(self,state_i):
         return self.goal.id==state_i.id
 
     def next_state(self,state_i):
-        raise NotImplemented()
+        pair_i=cantor_invert(state_i.id)
+        delta=[-1,0,1]
+        neigh=[ (pair_i[0]+x,pair_i[1]+y)
+                for x in delta
+                    for y in delta]
+        neigh=[ self.get_state(neig_i)
+                for neig_i in neigh
+                    if(self.valid(neig_i))]
+        return neigh
 
     def get_state(self, pair):
         state_id=cantor_paring(pair)
         if(not state_id in self.states):
             self.states[state_id]=search.State(state_id)
         return self.states[state_id]
+    
+    def valid(self,pair):
+        if(pair[0]<0 or pair[1]<0):
+            return False
+        bound=[ value>=bound  
+        	        for value,bound in zip(pair,self.grid.shape)]
+        if(any(bound)):
+        	return False
+        return self.grid[pair[0],pair[1]]==0
+
 
 def cantor_paring(k):
-    return (k[0]+k[1])*(k[0]+k[1]+1)/2	
+    return (k[0]+k[1])*(k[0]+k[1]+1)/2 + k[1]
+
+def cantor_invert(z):
+    w=np.floor( (np.sqrt(8*z+1)-1)/2 )
+    t= (w*w+w)/2
+    y=z-t
+    x=w-y
+    return int(x),int(y)
 
 def read_grid(in_path):
     with open (in_path, "r") as grid_file:
@@ -40,4 +65,6 @@ def read_grid(in_path):
         return GridSearch(text)
 
 gs=read_grid("sample.txt")
-gs((0,0),(5,3))   
+print(gs((0,0),(5,3)))
+#print(gs.valid((0,0)))
+#print(cantor_invert(cantor_paring((3,2))))
