c5455cd518fff1ed9eedd5ddee32edb8caaa1d2b
tjacek <tjacek@agh.edu.pl>
2020-10-06 16:46:44 +0200

    point in polygon

--- world.py
+++ world.py
@@ -1,6 +1,30 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+class Polygon(object):
+    def __init__(self,vertices):
+        self.vertices=vertices
+
+    def __len__(self):
+        return self.vertices.shape[0]
+
+    def __call__(self,point):
+        size=len(self)
+        cord=[(i,i+1) for i in range(size-1)]
+        cord.append((size-1,0))
+        inside=False
+        x,y=point[0],point[1]
+        for cord_i in cord:
+            print(cord_i)
+            b_i=self.vertices[cord_i[0]]
+            e_i=self.vertices[cord_i[1]]
+            cross_i=(b_i[0]<=x and e_i[0]>=x) or (e_i[0]<=x and b_i[0]>=x)
+#x < ((xj - xi) * (y - yi) / (yj - yi) + xi)
+            left_i= x<((e_i[0]-b_i[0])*(y-b_i[1])/(e_i[1]-b_i[1]) +b_i[0])
+            if(cord_i and left_i):
+                inside=not inside
+        return inside
+
 class ConvexPolygon(object):
     def __init__(self,half_planes):
         self.half_planes=half_planes
@@ -30,8 +54,12 @@ def plot_polygon(polygon,n=100,scale=100):
     plt.scatter(points[:,0],points[:,1])
     plt.show()
 
+#vertices=np.array([[-1,1],[0,0],[1,1],[1,-1],[-1,-1]])
 
-a=[ HalfPlane(1.0,1.0,1.0),HalfPlane(1.0,1.0,-1.0),
-    HalfPlane(-1.0,1.0,1.0),HalfPlane(-1.0,1.0,-1.0)]
-polygon=ConvexPolygon(a)
-plot_polygon(polygon,n=1000)
+vertices=np.array([[-1,1],[1,1],[1,-1],[-1,-1]])
+polygon=Polygon(vertices)
+print(polygon(np.array([100.0,-0.5])))
+#a=[ HalfPlane(1.0,1.0,1.0),HalfPlane(1.0,1.0,-1.0),
+#    HalfPlane(-1.0,1.0,1.0),HalfPlane(-1.0,1.0,-1.0)]
+#polygon=ConvexPolygon(a)
+#plot_polygon(polygon,n=1000)
