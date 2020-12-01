d4c3e670e9d9c0433688bb3b9ada3e5e49c2342e
tjacek <tjacek@agh.edu.pl>
2020-12-01 23:41:10 +0100

    polygon collision

--- world2D/__init__.py
+++ world2D/__init__.py
@@ -13,3 +13,12 @@ class RigidMotion(object):
 
     def __call__(self,point):
         return self.a.dot(point)+self.b
+
+def polygon_collision(pol1,pol2):
+	for ver_i in pol1.vertices:
+		if(pol2(ver_i)):
+			return True
+	for ver_i in pol2.vertices:
+		if(pol1(ver_i)):
+			return True
+	return False
