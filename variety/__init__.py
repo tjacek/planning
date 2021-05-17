e8c6fbe84a31857e0596185c9659f57e42380783
tjacek <tjacek@agh.edu.pl>
2021-05-17 15:14:00 +0200

    new make_pol

--- variety/__init__.py
+++ variety/__init__.py
@@ -17,14 +17,26 @@ class Variety(object):
 		return all([ cond_i(value_i) 
 				for value_i,cond_i in zip(values,self.conds)])
 
-def simple_cond(pol_value):
-	return pol_value <0
+def less(pol_value):
+	return pol_value<0
 		
-def make_pol():
-	p0=sympy.Poly(x**2+y**2-1,x,y)
-	p1=sympy.Poly(x**2-y,x,y)
-	conds=[simple_cond,simple_cond]
-	return Variety([x,y],[p0,p1],conds)
+def more(pol_value):
+	return pol_value>0
+
+def eq(pol_value):
+	return pol_value==0
+
+def make_pol(r1=5,r2=1,r3=1,
+			x2=2,y2=2.5,x3=-2,y3=2.5,
+			y4=-3,a=2.0,b=1 ):
+	f0=sympy.Poly(x**2+y**2-r1**2,x,y)
+	f1=sympy.Poly((x-x2)**2+(y-y2)**2-r2**2,x,y)
+	f2=sympy.Poly((x-x3)**2+(y-y3)**2-r3**2,x,y)
+	a,b=1/a,1/b
+	f3=sympy.Poly((a*x)**2+(b*(y-y4))**2-1,x,y)
+	pols=[f0,f1,f2,f3]
+	conds=[less,less,less,less]
+	return Variety([x,y],pols,conds)
 
 if __name__ == "__main__":
 	variety=make_pol()
