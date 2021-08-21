20381aec89047026515d50c1197182724ec5a6bf
tjacek <tjacek@agh.edu.pl>
2021-08-21 02:45:54 +0200

    polygon collision

--- world2D/collision.py
+++ world2D/collision.py
@@ -41,10 +41,31 @@ class PolygonEnvir(object):
         return [pol_i.vertices for pol_i in self.obstacles]
 
 def polygon_collision(pol1,pol2):
-    for ver_i in pol1.vertices:
-        if(pol2(ver_i)):
-            return True
-    for ver_i in pol2.vertices:
-        if(pol1(ver_i)):
-            return True
-    return False
+    edges =  pol1.as_edges() + pol2.as_edges()
+    axes = [get_normal(edge) for edge in edges]
+    for axis_i in axes:
+        proj1=project(pol1, axis_i)
+        proj2=project(pol1, axis_i)
+        overlapping = overlap(proj1, proj2)
+        if not overlapping:
+            return False
+    return True	
+
+def get_normal(vector):
+    vector=np.array([vector[1], -vector[0]])
+    norm = np.sqrt( np.sum(vector**2))
+    return vector / norm
+
+def project(polygon, axis_i):
+    dots = [np.dot(vertex, axis_i) for vertex in polygon.vertices]
+    return [np.min(dots), np.max(dots)]
+
+def overlap(proj1, proj2):
+    return min(proj1) <= max(proj2) and min(proj2) <= max(proj1)
+
+if __name__ == "__main__":
+    a=convex.ConvexPolygon([[-2,-1],[2,-1],[2,1],[-2,1]])
+    b=convex.ConvexPolygon([[-1,-2],[-1,2],[1,2],[1,-2]])
+    print(polygon_collision(a,b))
+#    import plot
+#    plot.plot_polygon([a,b])
