62ef8c244f45004bc9bb351ec52f5351a1a2cef3
tjacek <tjacek@agh.edu.pl>
2021-08-21 20:53:09 +0200

    sample function

--- world2D/__init__.py
+++ world2D/__init__.py
@@ -6,26 +6,19 @@ class Problem(object):
         self.start=start
         self.end=end
         self.collision=collision
+        self.sample=sample_naive
 
     def get_box(self):
         box1=self.start.get_box()
         box2=self.end.get_box()
         box3=self.collision.get_box()
         return box1+box2+box3
-
-    def sample(self,n):
-        bounds=self.get_box()
-        x=np.random.uniform(bounds.min[0],bounds.max[0],size=n)
-        y=np.random.uniform(bounds.min[1],bounds.max[1],size=n)
-        theta=np.random.uniform(0,2*np.pi,size=n)
-        if(n==1):
-            return RigidMotion(theta[0],x[0],y[0])
-        return [ RigidMotion(theta[i],x[i],y[i]) for i in range(n)]
-
+        
     def legal(self,n,iters=100):
+        bounds=self.get_box()
         positions,motions=[],[]
         while(n>0):
-            motion_i=self.sample(1)
+            motion_i=self.sample(bounds,1)
             position_i=self.start.move(motion_i)
             if(not self.collision(position_i)):
                 motions.append(motion_i)	
@@ -63,3 +56,11 @@ def read_problem(in_path):
             for ver_i in raw["collision"]]
     pol_envir=collision.PolygonEnvir(obstacles)
     return Problem(start,end,pol_envir)
+
+def sample_naive(bounds,n):
+    x=np.random.uniform(bounds.min[0],bounds.max[0],size=n)
+    y=np.random.uniform(bounds.min[1],bounds.max[1],size=n)
+    theta=np.random.uniform(0,2*np.pi,size=n)
+    if(n==1):
+        return RigidMotion(theta[0],x[0],y[0])
+    return [ RigidMotion(theta[i],x[i],y[i]) for i in range(n)]
