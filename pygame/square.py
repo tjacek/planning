38c92dcd1b2d0207f634862f0103d55a42e58d18
tjacek <tjacek@agh.edu.pl>
2021-09-07 17:51:01 +0200

    random squares

--- pygame/square.py
+++ pygame/square.py
@@ -1,17 +1,28 @@
+import numpy as np
 import pygame
 
 class Envir(object):
     def __init__(self,obstacles):
         self.obstacles=obstacles
 
+    def show(self,window,color=(255,255,255)):
+        for rect_i in envir.obstacles:
+            pygame.draw.rect(window, color, rect_i)
+
 pygame.init()
 window = pygame.display.set_mode((250, 250))
 
-obstacles=[(50,100,10,10),(10,10,10,10),(40,40,10,10),(60,30,10,10)]
-rects=[]
+def rect_world(n_rect,bounds=(5,10),world=(130,150)):
+    rects=[]
+    for i in range(n_rect):
+        width,height=np.random.uniform(bounds[0],bounds[1],2)
+        x=np.random.uniform(0.0,world[0])
+        y=np.random.uniform(0.0,world[1])
+        rects.append(pygame.Rect(x,y,width,height))
+    return Envir(rects)
 
-for x,y,width,height in obstacles:
-    rects.append(pygame.Rect(x,y,width,height))  #pygame.Rect(*window.get_rect().center, x, y).inflate(width, height))
+
+envir=rect_world(5)
 
 run = True
 while run:
@@ -19,13 +30,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-#    point = pygame.mouse.get_pos()
-#    collide = rect.collidepoint(point)
-#    color = (255, 0, 0) if collide else (255, 255, 255)
-    color=(255,255,255)
+#    color=(255,255,255)
     window.fill(0)
-    for rect_i in rects:
-        pygame.draw.rect(window, color, rect_i)
+    envir.show(window)
     pygame.display.flip()
 
 pygame.quit()
