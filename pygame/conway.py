ca54d115e0255b852da6fbbddb7c142bb95e6fec
tjacek <tjacek@agh.edu.pl>
2021-09-23 21:26:42 +0200

    conway

---
+++ pygame/conway.py
@@ -0,0 +1,40 @@
+import numpy as np
+import grid
+
+class ConwayControler(object):
+    def __init__(self,grid):
+        self.grid=grid
+
+    def on_click(self,point):
+        self.next_step()
+
+    def on_key(self,key):
+        self.next_step()
+    
+    def next_step(self):
+        x,y=self.grid.x,self.grid.y
+        next_state=[[ self.is_alive(i,j)
+                        for i in range(x)]
+                            for j in range(y)]
+        next_state=np.array(next_state)
+        for cord, alive in np.ndenumerate(next_state):
+            if(alive):
+                self.grid.set_color(cord,grid.CellColors.obst)
+            else: 
+                self.grid.set_color(cord,grid.CellColors.empty)
+
+    def is_alive(self,i,j):
+        n_alive=8-len(self.grid.near(i,j))
+        active=self.grid[(i,j)].active()
+        if(active and n_alive==3):
+            return True
+        if(not active and (n_alive==2 or n_alive==3)):
+            return True
+        return False
+
+def game_of_life(in_path,step=40):
+    raw_grid=grid.read_grid(in_path,step)
+    controler=ConwayControler(raw_grid)
+    grid.grid_loop(controler)
+
+game_of_life("test.txt")
