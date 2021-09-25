97f6bc4eba26e13c8068f72c79863eeb1c00e7f7
tjacek <tjacek@agh.edu.pl>
2021-09-25 19:22:42 +0200

    graph module

---
+++ pygame/graph.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+class GridGraph(object):
+    def __init__(self,vertices):
+        self.vertices=vertices
+        self.goal=None
+
+    def __getitem__(self,cord):
+        return self.vertices[cantor_paring(cord)]
+
+class Vertex(object):
+    def __init__(self,cord,edges=[]):
+        self.cord=cord
+        self.edges=edges
+        self.visited=False
+        self.parent=None
+        self.cost=np.inf
+
+    def reset(self):
+        self.visited=False
+        self.parent=None
+        self.cost=np.inf
+
+class DijkstraSearch(GridGraph):
+    def find_path(self,start):
+        if(type(start)==tuple):
+            start=self[start]
+        return get_path(start)	
+
+    def set_goal(self,goal):
+        if(type(goal)==tuple):
+            goal=self[goal]
+        self.goal=goal
+        self.dijkstra(self.goal)
+
+    def dijkstra(self,end):
+        self.reset()
+        states=list(self.vertices.values())
+        end.cost=0
+        while(states):
+            states.sort(key=lambda x:x.cost,reverse=True)
+            state_i=self[states.pop().cord]
+            for near_j in state_i.edges:
+            	near_j=self[near_j]
+            	if(near_j.cost>(state_i.cost+1)):
+                    near_j.cost=state_i.cost+1
+                    near_j.parent=state_i
+
+    def reset(self):
+        for vertex_i in self.vertices.values():
+            vertex_i.reset()	
+
+class FowardSearch(GridGraph):
+    def set_goal(self,goal):
+        if(type(goal)==tuple):
+            goal=self[goal]
+        self.goal=goal
+
+    def find_path(self,start):
+        if(type(start)==tuple):
+            start=self[start]
+        Q=[start]
+        while(Q):
+            x=Q.pop()
+            if(x==self.goal):
+                return get_path(start)
+            for edge_i in x.edges:
+                x_i=self[edge_i]
+                if(not x_i.visited):
+                    x_i.visited=True
+                    Q.append(x_i)
+        return False
+
+def cantor_paring(k):
+    return (k[0]+k[1])*(k[0]+k[1]+1)/2 + k[1]
+
+def get_grid_graph(raw_grid):
+    vertices={}
+    for i in range(raw_grid.x):
+        for j in range(raw_grid.y):
+            cord=(i,j)
+            key_ij=cantor_paring(cord)
+            vertex_ij=Vertex(cord,raw_grid.near(*cord))
+            vertices[key_ij]=vertex_ij
+    return vertices
+
+def get_path(start):
+    path=[]
+    vertex_i=start
+    while(vertex_i):
+        path.append(vertex_i)
+        vertex_i=vertex_i.parent
+    print(len(path))
+    return path
