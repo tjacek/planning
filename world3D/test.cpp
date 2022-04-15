785f90154c454ff5e322c244081f678dd69864b8
tjacek <tjacek@agh.edu.pl>
2022-04-15 14:21:06 +0200

    world3D

---
+++ world3D/test.cpp
@@ -0,0 +1,30 @@
+#include "mujoco.h"
+#include "stdio.h"
+
+char error[1000];
+mjModel* m;
+mjData* d;
+
+int main(void)
+{
+   // load model from file and check for errors
+   m = mj_loadXML("hello.xml", NULL, error, 1000);
+   if( !m )
+   {
+      printf("%s\n", error);
+      return 1;
+   }
+
+   // make data corresponding to model
+   d = mj_makeData(m);
+
+   // run simulation for 10 seconds
+   while( d->time<10 )
+      mj_step(m, d);
+
+   // free model and data
+   mj_deleteData(d);
+   mj_deleteModel(m);
+
+   return 0;
+}
