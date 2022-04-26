91017a8d39de4c3af2b17267b4a85bfd71abaaa2
tjacek <tjacek@agh.edu.pl>
2022-04-26 20:36:18 +0200

    random envir

--- localization/kalman.py
+++ localization/kalman.py
@@ -9,6 +9,9 @@ class Envir(object):
         self.cov_w=cov_w 
         self.state=None
 
+    def get_dim(self):
+        return self.A.shape[0]
+
     def set_state(self,state):
     	self.state=state
 
@@ -17,3 +20,14 @@ class Envir(object):
 
     def observe(self):
         return self.C.dot(self.state)
+
+def random_envir(dim=3):
+    A=2*np.random.rand(dim,dim)
+    B=np.random.rand(dim,dim)
+    C=np.random.rand(dim,dim)
+    cov_v=np.random.rand(dim,dim)
+    cov_w=np.random.rand(dim,dim)
+    return Envir(A,B,C,cov_v,cov_w)
+
+envir=random_envir()
+print(envir.get_dim())
