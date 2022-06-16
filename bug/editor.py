4898f009265bddbd47f3f1fe764645146a5ebbbe
tjacek <tjacek@agh.edu.pl>
2022-06-16 20:17:33 +0200

    goals

--- bug/editor.py
+++ bug/editor.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from enum import Enum 
 from scipy.spatial import ConvexHull
 import numpy as np
 import json
@@ -9,7 +10,6 @@ class World(object):
 
     def add_polygon(self,points:list):
         polygon_i=make_polygon(points)
-        polygon_i.rotate(1.0)
         self.polygons.append(polygon_i)
 
     def show(self,window):
@@ -38,25 +38,54 @@ class Polygon(object):
     def show(self,window):
         pg.draw.polygon(window,(0,128,0),self.vertices)
 
+class EditorMode(Enum):
+    OBSTACLE = 1
+    START = 2
+    END = 3
+
 class Editor(object):
     def __init__(self):
-        self.world=World()
+        self.world=World()        
         self.points=[]
+        self.mode=EditorMode.OBSTACLE
+        self.start=None
+        self.end=None
 
     def on_click(self,point):
-        self.points.append(point)
-        print(len(self.points))	
+        if(self.mode==EditorMode.OBSTACLE):
+            self.points.append(point)
+            print(len(self.points))	
+        if(self.mode==EditorMode.START):
+            self.start=point
+        if(self.mode==EditorMode.END):
+            self.end=point
 
     def on_key(self,key):
-        if(len(self.points)>2):
-            self.world.add_polygon(self.points)
-            self.points.clear()
+        print(key)
+        if(key==115):
+            self.mode=EditorMode.START
+        elif(key==101):
+            self.mode=EditorMode.END
+        else:
+            self.mode=EditorMode.OBSTACLE
+            if(len(self.points)>2):
+                self.world.add_polygon(self.points)
+                self.points.clear()
 
     def show(self,window):
         window.fill((0,0,0))
+        if(self.start):
+            pg.draw.circle(window,(0,64,64), self.start, 5)
+        if(self.end):
+            pg.draw.circle(window,(64,0,64), self.end, 5)        
         for point_i in self.points:
             pg.draw.circle(window,(0,0,128), point_i, 5)
+        self.solve(window)
         self.world.show(window)
+
+    def solve(self,window):
+        if(self.start and self.end):
+            pg.draw.line(window,(255,255,0),self.start,self.end)
 
 def save_json(data,out_path):
     def helper(obj):
