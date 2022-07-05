0a5a43084e207f23978b7cc873767215c941cd7c
tjacek <tjacek@agh.edu.pl>
2022-07-05 22:15:42 +0200

    VertexIterator class

--- bug/examine.py
+++ bug/examine.py
@@ -2,12 +2,31 @@ import sys
 import pygame as pg
 import world,editor
 
+class VertexIterator(object):
+    def __init__(self,world):
+        self.world=world
+        self.n_polygon=0
+        self.n_vertex=0
+
+    def next(self):
+        pol_i=self.world.polygons[self.n_polygon]
+        self.n_vertex= (self.n_vertex+1) % len(pol_i)
+        if(self.n_vertex==0):
+            self.n_polygon= (self.n_polygon+1) % len(self.world)
+
+    def current(self):
+        pol_i=self.world.polygons[self.n_polygon]
+        vertex_j=pol_i.vertices[self.n_vertex]
+        return vertex_j
+
+    def __str__(self):
+        return f"{self.n_polygon},{self.n_vertex}"
+
 class VertexControler(object):
     def __init__(self,problem):
         self.problem=problem
         self.mode=editor.EditorMode.START
-        self.n_polygon=0
-        self.n_vertex=0
+        self.vertices=VertexIterator(problem.world)
 
     def on_click(self,point):
         if(self.mode==editor.EditorMode.START):
@@ -16,25 +35,21 @@ class VertexControler(object):
             self.problem.end=point
 
     def on_key(self,key):
-        print((self.n_polygon,self.n_vertex))
+        print(self.vertices)
         print(key)
         if(key==115):
             self.mode=editor.EditorMode.END
         elif(key==101):
             self.mode=editor.EditorMode.START
         else:
-            pol_i=self.problem.world.polygons[self.n_polygon]
-            self.n_vertex= (self.n_vertex+1) % len(pol_i)
-            if(self.n_vertex==0):
-                self.n_polygon= (self.n_polygon+1) % len(self.problem.world)
+            self.vertices.next()
         print(self.mode)
 
     def show(self,window):
         window.fill((0,0,0))
         self.problem.world.show(window)
         self.problem.show(window)
-        pol_i=self.problem.world.polygons[self.n_polygon]
-        vertex_j=pol_i.vertices[self.n_vertex]
+        vertex_j=self.vertices.current()
         pg.draw.circle(window,(0,0,128), vertex_j, 5) 
 #        segments= pol_i.get_segments()
 #        x,y=segments[self.n_vertex]
