34dee12f7d99faf7af408a14f6aabee3c765e61f
tjacek <tjacek@agh.edu.pl>
2022-09-22 13:46:56 +0200

    colision free lines

--- bug/vgraph.py
+++ bug/vgraph.py
@@ -1,14 +1,31 @@
+import itertools
 import world,editor,demo
 
 def viz_graph(problem):
-    print(problem)
-    vertices=problem.world.all_vertices()
-    n_vertices=len(vertices)
+    world=problem.world
+    lines=[line_i 
+        for line_i in inter_polgon_vert(world)
+          if(len(world.collision(line_i))==0)]
+    return lines
+#    vertices=world.all_vertices()
+#    lines=[]
+#    for i,ver_i in enumerate(vertices):
+#        for j,ver_j in enumerate(vertices):
+#            if(i!=j):
+#                line_ij=ver_i,ver_j
+#                col=world.collision(line_ij)
+#                if(len(col)==0):
+#                    lines.append(line_ij)
+#    return lines
+
+def inter_polgon_vert(world):
+    indexes=range(len(world))
+    pol_pairs=itertools.combinations(indexes,2)
     lines=[]
-    for i in range(n_vertices):
-        for j in range(n_vertices):
-            if(i!=j):
-                lines.append((vertices[i],vertices[j]))
+    for i,j in pol_pairs:
+        pol_i=world.polygons[i].vertices
+        pol_j= world.polygons[j].vertices
+        lines+=list(itertools.product(pol_i,pol_j))
     return lines
 
 def vgraph_loop(in_path):
@@ -17,4 +34,4 @@ def vgraph_loop(in_path):
     editor.loop_template(controler)
 
 
-vgraph_loop("vis.json")
+vgraph_loop("test.json")#"vis.json")
--- bug/world.py
+++ bug/world.py
@@ -69,11 +69,11 @@ class World(object):
         save_json(data,out_path)
         print(f"save at {out_path}")
 
-    def all_vertices(self):
-        all_verices=[]
-        for pol_i in self.polygons:
-            all_verices.append( pol_i.vertices)
-        return np.concatenate(all_verices,axis=0)
+#    def all_vertices(self):
+#        all_verices=[]
+#        for pol_i in self.polygons:
+#            all_verices.append( pol_i.vertices)
+#        return np.concatenate(all_verices,axis=0)
 
 def save_json(data,out_path):
     def helper(obj):
