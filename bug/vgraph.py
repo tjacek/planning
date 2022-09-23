5f3e12c1bc130a9795a1351a4df2161cd49fc6ee
tjacek <tjacek@agh.edu.pl>
2022-09-23 17:57:22 +0200

    closest_vertex

--- bug/vgraph.py
+++ bug/vgraph.py
@@ -1,3 +1,4 @@
+import numpy as np
 import itertools
 import world,editor,demo
 
@@ -20,30 +21,45 @@ class VizGraph(object):
             if(i is None):
                 size=len(self.nodes)
                 self.near_edeges.append([edge])
-                self.nodes.append(x)
+                self.nodes.append(np.array(x))
             else:
                 self.near_edeges[i].append(edge)
         self.edges.append(edge)
 
+    def closest_vertex(self,vertex,world):
+        distance=[np.linalg.norm(vertex-node_i) 
+                    for node_i in self.nodes]
+        for i in np.argsort(distance):
+            line_i=(vertex,self.nodes[i])
+            if(len(world.collision(line_i))==0):
+                return line_i
+        raise Exception('should never happen')
+
+    def find_path(self,start,end):
+        lines=[]
+        current=self.get_index(start)
+#        goal
+        return lines
+
 def viz_graph(problem):
     graph=VizGraph()
     world=problem.world
-    for line_i in inter_polgon_vert(world):        
+    lines= world.all_segments()+list(inter_polgon_vert(world))
+    for line_i in lines:        
         if(len(world.collision(line_i))==0):
             graph.add_edge(line_i)
-    return graph.near_edeges[0]
+    start=graph.closest_vertex(problem.start,world)
+    end=graph.closest_vertex(problem.end,world)
+    return [start,end]#graph.edges
 
 def inter_polgon_vert(world):
     indexes=range(len(world))
     pol_pairs=itertools.combinations(indexes,2)
-#    lines=[]
     for i,j in pol_pairs:
         pol_i=world.polygons[i].vertices
         pol_j= world.polygons[j].vertices
         for line_i in itertools.product(pol_i,pol_j):
             yield line_i
-#        lines+=list(itertools.product(pol_i,pol_j))
-#    return lines
 
 def vgraph_loop(in_path):
     problem=world.read_json(in_path)
--- bug/world.py
+++ bug/world.py
@@ -69,11 +69,11 @@ class World(object):
         save_json(data,out_path)
         print(f"save at {out_path}")
 
-#    def all_vertices(self):
-#        all_verices=[]
-#        for pol_i in self.polygons:
-#            all_verices.append( pol_i.vertices)
-#        return np.concatenate(all_verices,axis=0)
+    def all_segments(self):
+        segments=[]
+        for pol_i in self.polygons:
+            segments+= pol_i.get_segments()
+        return segments
 
 def save_json(data,out_path):
     def helper(obj):
