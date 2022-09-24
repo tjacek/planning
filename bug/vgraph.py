bcd2311540245054ea2ce848c2a56708a49da539
tjacek <tjacek@agh.edu.pl>
2022-09-24 14:24:51 +0200

    VizGraphAlg

--- bug/vgraph.py
+++ bug/vgraph.py
@@ -62,22 +62,33 @@ class VizGraph(object):
                         active.append(i)
             visited[current]=True
         path=[]
-        print(cost)       
-#        goal
+        current=end
+        while(not (parent[current] is None)):
+            line_i=(self.nodes[current],self.nodes[parent[current]])
+            path.append(line_i)
+            current=parent[current]
         return path
 
-def viz_graph(problem):
-    graph=VizGraph()
-    world=problem.world
-    lines= world.all_segments()+list(inter_polgon_vert(world))
-    for line_i in lines:        
-        if(len(world.collision(line_i))==0):
-            graph.add_edge(line_i)
-    start=graph.closest_vertex(problem.start,world)
-    end=graph.closest_vertex(problem.end,world)
-    graph.find_path(start[1],end[1])
-#    print(graph.edges[0])
-    return [start,end]#graph.edges
+class VizGraphAlg(object):
+    def __init__(self):
+        self.graph=None
+
+    def __call__(self,problem):
+        world=problem.world
+        if(self.graph is None):
+            self.build_graph()
+        start=self.graph.closest_vertex(problem.start,world)
+        end=self.graph.closest_vertex(problem.end,world)
+        path=self.graph.find_path(start[1],end[1])
+        return [start]+path+[end]
+
+    def build_graph(self):
+        self.graph=VizGraph()
+        lines= world.all_segments()
+        lines+=list(inter_polgon_vert(world))
+        for line_i in lines:        
+            if(len(world.collision(line_i))==0):
+                self.graph.add_edge(line_i)
 
 def inter_polgon_vert(world):
     indexes=range(len(world))
@@ -90,6 +101,7 @@ def inter_polgon_vert(world):
 
 def vgraph_loop(in_path):
     problem=world.read_json(in_path)
+    viz_graph=VizGraphAlg()
     controler= demo.AlgControler(problem,viz_graph)
     editor.loop_template(controler)
 
