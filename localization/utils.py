1e1ac0b29cd4c9657e298c8cbaf1713d8b6c2630
tjacek <tjacek@agh.edu.pl>
2023-09-02 23:30:06 +0200

    compute likehood

--- localization/utils.py
+++ localization/utils.py
@@ -15,8 +15,17 @@ class Gauss(object):
     def __call__(self):
         return np.random.multivariate_normal(self.mean,self.cov)
     
+    def likehood(self,x):
+        sigma=np.linalg.inv(self.cov) 
+        return np.exp(-0.5((x-self.cov).T @ sigma @ (x-self.cov)))
+
     def __str__(self):
         return f'Gauss{sel.conv.shape[0]}D'
         
 def is_number(var):
     return (type(var) == int or type(var) == float)
+
+def rot_matrix(theta):
+    mat=[[np.cos(theta), -np.sin(theta)],
+         [np.sin(theta),  np.cos(theta)]]
+    return np.array(mat)
