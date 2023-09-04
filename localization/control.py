d965367fb6731efb305215379c5feebdaff76992
tjacek <tjacek@agh.edu.pl>
2023-09-04 21:13:00 +0200

    BasicBounds

--- localization/control.py
+++ localization/control.py
@@ -5,17 +5,19 @@ import model,show
 class MotionControler(object):
     def __init__(self,exp,bounds=(256,256)):
         self.exp=exp
-        self.bounds=np.array(bounds)
-        self.scale=[1.0,1.0]
+#        self.bounds=np.array(bounds)
+        self.bounds=BasicBounds()
+#        self.scale=[1.0,1.0]
         self.action=MotionAction()
         self.current=None
 
-    def get_bounds(self):
-        return 2*self.bounds
+#    def get_bounds(self):
+#        return 2*self.bounds
 
     def on_click(self,point):
-        new_state=[ (point_i-bound_i) 
-            for point_i,bound_i in zip(point,self.bounds)]
+#        new_state=[ (point_i-bound_i) 
+#            for point_i,bound_i in zip(point,self.bounds)]
+        new_state=self.bounds.get_state(point)
         new_state=np.array(new_state+[0,0],dtype=float)
         self.exp.envir.state=new_state
         print(str(self.exp.envir))
@@ -32,19 +34,22 @@ class MotionControler(object):
 
     def show(self,window):
         window.fill((0,0,0))
-        self.draw_lines(window,step=self.scale[0]*64,horiz=True)
-        self.draw_lines(window,step=self.scale[1]*64,horiz=False)
+        step=64*self.bounds.scale
+        self.draw_lines(window,step=step[0],#self.scale[0]*64,
+                        horiz=True)
+        self.draw_lines(window,step=step[1],#self.scale[1]*64,
+                        horiz=False)
         
 
         if(self.current is None):
             return
 
         true_state,obs_state,pred_state=self.current   
-        obs_state=self.transform_point(obs_state)
+        obs_state=self.bounds.transform_point(obs_state)
         pg.draw.circle(window,(128,0,0),obs_state,10)
 
         x,y,theta,v= true_state        
-        true_state=self.transform_point([x,y])
+        true_state=self.bounds.transform_point([x,y])
         pg.draw.circle(window,(0,0,128),true_state,10)
         x,y=true_state
         pg.draw.line(window,
@@ -53,26 +58,11 @@ class MotionControler(object):
                      end_pos=(int(x+25*np.cos(theta)),
                               int(y+25*np.sin(theta))))
         
-        pred_state=self.transform_point(pred_state[:2])
+        pred_state=self.bounds.transform_point(pred_state[:2])
         pg.draw.circle(window,(0,128,0),pred_state,10)
 
-    def transform_point(self,point):
-        point=[self.rescale(p_i,i)*p_i 
-                for i,p_i in enumerate(point)]
-        point=np.array(point)
-        point+=self.bounds
-        return point
-
-    def rescale(self,cord,i):
-        if( cord<-self.bounds[i] or 
-          self.bounds[i]<cord):
-            self.scale[i]=0.9*(self.bounds[i]/np.abs(cord))
-        else:
-            self.scale[i]=1
-        return self.scale[i]
-
     def draw_lines(self,window,step=64,horiz=True,color=(0,128,0)):
-        bounds=self.get_bounds()
+        bounds=self.bounds.get_bounds()
         n_lines= int(bounds[0]/step)    
         for i in range(n_lines):
             x_i= i*step
@@ -109,6 +99,33 @@ class MotionAction(object):
 
     def __str__(self):
         return f'v:{self.u[0]:4f},omega:{self.u[1]:4f}'
+
+class BasicBounds(object):
+    def __init__(self,bounds=(256,256)):
+        self.bounds=np.array(bounds)
+        self.scale=np.array([1.0,1.0])
+
+    def get_bounds(self):
+        return 2*self.bounds
+
+    def get_state(self,point):
+        return[ (point_i-bound_i) 
+                for point_i,bound_i in zip(point,self.bounds)]
+
+    def transform_point(self,point):
+        point=[self.rescale(p_i,i)*p_i 
+                for i,p_i in enumerate(point)]
+        point=np.array(point)
+        point+=self.bounds
+        return point
+
+    def rescale(self,cord,i):
+        if( cord<-self.bounds[i] or 
+          self.bounds[i]<cord):
+            self.scale[i]=0.9*(self.bounds[i]/np.abs(cord))
+        else:
+            self.scale[i]=1
+        return self.scale[i]
 
 if __name__ == '__main__':
     exp=model.Experiment(envir=model.simple_motion_model(),
